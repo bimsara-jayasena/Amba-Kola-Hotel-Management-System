8c42ca6d931848b5612f093887165fca022d08cd
Bimsara Jayasena <129860594+bimsara-jayasena@users.noreply.github.com>
2024-10-01 17:48:25 +0530

    Delete Client/react-app/my-app directory

--- Client/react-app/my-app/src/theme/customizations/index.tsx
+++
@@ -1,4 +0,0 @@
-export { chartsCustomizations } from './chart';
-export { dataGridCustomizations } from './dataGrid';
-export { datePickersCustomizations } from './datePickers';
-export { treeViewCustomizations } from './treeView';
